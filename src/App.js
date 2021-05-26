c30feb4f38356aa3924d4869d3abff0137925ea6
eslazyk10 <eslazy32@gmail.com>
2021-05-26 13:28:08 -0500

    added content to Header.js and styled with css
    imported icons from material ui and created sidebar component to begin working on body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./Header";
+import Sidebar from "./Sidebar";
+
 import './css/App.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>Let's build!</h1>
+      <Sidebar />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { Avatar, IconButton } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
-import './css/Header.css'
+import './css/Header.css';
 
 export default function Header(){
     return (
         <div className="header">
-            
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="Gmail logo" />
+            </div>
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder="Search Mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret" />
+            </div>
+            <div className="header__right">
+                <AppsIcon />
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+
         </div>
     )
 }
