31764c1aa7aab0250dfd23da946486882a36903e
eslazyk10 <eslazy32@gmail.com>
2021-05-26 12:49:12 -0500

    created react app with redux template
    cleared non-essential folders/files, created Header components, created folder for css files, imported material ui from npm

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <h1>Let's build!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import './css/Header.css'
+
+export default function Header(){
+    return (
+        <div className="header">
+            
+        </div>
+    )
+}
